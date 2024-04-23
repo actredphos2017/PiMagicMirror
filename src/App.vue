0b20869fabf824928602df866f77437e4a84b34b
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-04-23 19:25:22 +0800

    分上下布局

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,38 @@ const testStructure: Compose[] = [
 </script>
 
 <template>
-  <dynamic-component :structure="testStructure"/>
+  <div class="main-container">
+    <div class="compose-area-container">
+      <dynamic-component :structure="testStructure"/>
+      <dynamic-component :structure="testStructure"/>
+    </div>
+    <div class="notify-area-container">
+      Hello World
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+.main-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  padding: 64px 0;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.compose-area-container {
+  width: calc(100% - 128px);
+  margin: 0 64px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.notify-area-container {
+  width: calc(100% - 128px);
+  margin: 0 64px;
+}
 
 </style>
