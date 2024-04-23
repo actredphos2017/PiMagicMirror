428b9a895e8be9399572cbbd13f6fd65bf493f4f
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-04-23 19:25:07 +0800

    增加组件边框

--- src/layouts/DynamicComponent.vue
+++ src/layouts/DynamicComponent.vue
@@ -13,17 +13,35 @@ const composes = computed(() => toComposeComponents(props.structure));
 </script>
 
 <template>
-  <div style="display: flex; flex-direction: column; gap: 12px">
-    <component
+  <div class="column-container">
+    <div
+        class="compose-wrapper"
         v-for="compose in composes"
         :key="compose.id"
-
-        :is="compose.component"
-        v-bind="compose.params"
-    />
+    >
+      <component
+          :is="compose.component"
+          v-bind="compose.params"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+.column-container {
+  display: flex;
+  flex-direction: column;
+  gap: 12px
+}
+
+.compose-wrapper {
+  width: 400px;
+  height: 200px;
+  padding: 12px;
+  border: #5b5b5b 2px solid;
+  border-radius: 12px;
+  box-shadow: #4f4f4f 0 0 12px;
+}
 
 </style>
