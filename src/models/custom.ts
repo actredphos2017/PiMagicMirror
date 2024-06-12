1b24b8f181bbba570bf582f6b18f07a21d2acf2c
Sakulin <actredphos2017@gmail.com>
2024-06-12 15:16:30 +0800

    添加: 记事与日程

--- src/models/custom.ts
+++ src/models/custom.ts
@@ -9,9 +9,26 @@ export interface UserProfile {
 
 export interface CustomSettings {
     compose_structure: ComposeStructure;
+    note: Notes;
+    schedule_list: ScheduleList;
+}
+
+export interface ScheduleList {
+    schedules: {
+        name: string;
+        content: string;
+        date: number;
+    }[];
 }
 
 export interface ComposeStructure {
     left: Compose[];
     right: Compose[];
+}
+
+export interface Notes {
+    notes: {
+        content: string;
+        create_time: number;
+    }[];
 }
